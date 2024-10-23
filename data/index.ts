fb166e99bc79e17b3410ae7417f820444176de44
FLRSscript <flrsscript@users.noreply.github.com>
2024-10-23 17:21:48 -0600

    portfolio actualizado

--- data/index.ts
+++ data/index.ts
@@ -1,8 +1,8 @@
 export const navItems = [
-  { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
-  { name: "Contact", link: "#contact" },
+  { name: "Info", link: "#about" },
+  { name: "Proyectos", link: "#projects" },
+  { name: "Reviews", link: "#testimonials" },
+  { name: "Contacto", link: "#contact" },
 ];
 
 export const gridItems = [
